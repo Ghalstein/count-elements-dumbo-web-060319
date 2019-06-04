2bdb9c19ec9b23ce91be3f744e09920b069ecb31
Garrett Halstein <garrett.halstein@emory.edu>
2019-06-04 09:01:30 -0400

    completed lab

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
-  # code goes here
+  count_hash = {}
+  array.each {|element|
+    if count_hash.include?(element)
+      count_hash[element] = 1 + count_hash[element]
+    else
+      count_hash[element] = 1
+    end
+  }
 end
  
